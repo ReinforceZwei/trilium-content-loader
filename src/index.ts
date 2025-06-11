31d6802b63d94ae50961146616373862e575fb35
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2025-06-11 13:40:57 +0800

    Add TriliumApi export to index for enhanced module accessibility

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { triliumLoader } from './TriliumContentLoader.js';
 export * from './types.js';
 export * from './ContentProcessor/index.js';
 export * from './renderer/index.js';
+export * from './TriliumApi.js';
